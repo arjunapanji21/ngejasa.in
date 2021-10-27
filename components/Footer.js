bacdf4bd41b17011e56154b9599799ce0d47170c
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-27 07:10:01 +0700

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,10 @@
+import Link from 'next/link';
 const Footer = () => {
     return (
       <footer>
+        <h2 className="toTop">
+          <Link href="/#"><a>Back to Top</a></Link>
+        </h2>
         &copy;2021 <a href="https://instagram.com/ngejasa.in/" target="_blank">ngejasa.in</a>
       </footer>
     );
